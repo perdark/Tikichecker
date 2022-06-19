79ced7d646257c024c95e96fe64df5de484b14d1
حسـين <106813922+B4B4B@users.noreply.github.com>
2022-06-19 21:12:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,14 +7,6 @@ from telebot import types
 bot_token ="5446689009:AAHhc54DpkslzukSj4kGQkaMvV70NEDbXFc"
 bot = telebot.TeleBot(bot_token)
 admin=[2029773594,5487311341,2053044330,1959561727,5244755240,2067768173,1972138234,1777649029,1824081699,1640747466,5244755240]
-@bot.message_handler(commands=["start"])
-def ss(message):
-    fs = message.from_user.first_name
-    bot.send_message(message.chat.id,f"""
-اهلا {fs}
-في بوت يوزرات تليجرام 
-
-للتأكد من الاشتراك بالبوت ارسل /TXN
 
 للاشتراك بالبوت راسل @E_4_1""")
 @bot.message_handler(commands=['TXN','txn','Txn'])
