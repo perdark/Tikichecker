cb9b20b0446a4f225fd87e80c48b7e3a5042eb53
حسـين <106813922+B4B4B@users.noreply.github.com>
2022-06-19 13:47:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ admin=[2029773594,1264770085,2053044330,1959561727,5244755240,2067768173,1972138
 @bot.message_handler(commands=['start'])
 def start_message(message):
     first = message.from_user.first_name
-    url = 'https://t.me/N1111V'
+    url = 'https://t.me/N1111V/3'
     bot.send_animation((message.chat.id), url, caption=f"""اهلا {first}
 
                 اهلا بك في بوت يوزرات تليجرام
